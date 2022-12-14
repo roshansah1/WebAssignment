ae1f0f7dbc8f844be96fe12d9a6cc7cf7075813c
Roshan sah <34867634+roshansah1@users.noreply.github.com>
2022-12-14 21:27:36 +0530

    Update main.js

--- 4-Advance-JS/410-Projects/AdvanceJS-410-04/main.js
+++ 4-Advance-JS/410-Projects/AdvanceJS-410-04/main.js
@@ -1,15 +1,24 @@
-
 let input = document.getElementById('input');
-
 let button = document.getElementById('button');
+
+
+input.addEventListener('keyup', (e) => {
+    let city = e.target.value;
+    if(e.keyCode == 13)
+    getWeather(city)
+})
 
 button.addEventListener('click', () => {
     let city = input.value;
+    getWeather(city)
+})
+
+function getWeather(city) {
     input.value = ""
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d8ab14f7e93c56cbe84562d28e8202bd&units=metric`)
     .then(res => res.json())
     .then((data) => {
-        console.log(data)
+       // console.log(data)
         let parent = document.getElementById('content');
 
         parent.innerHTML = `<div class="weather_box">
@@ -21,4 +30,4 @@ button.addEventListener('click', () => {
         </div>
     </div>`
     })
-})
+}
